4603dd0327c6829ffa66dfb2beefd25462e1e41d
Cristaliov Nichita <cristaliov.nichita@gmail.com>
2023-01-12 16:10:09 +0100

    ex4

--- com/centraleNantes/poei2/nichita/Main.java
+++ com/centraleNantes/poei2/nichita/Main.java
@@ -1,14 +1,15 @@
 package com.centraleNantes.poei2.nichita;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//ex1();
-		ex2();
+		//ex2();
+		//ex3();
+		ex4();
+
 	}
 	//ex1
 	public static void ex1(){
@@ -25,9 +26,9 @@ public class Main {
 	public static void ex2(){
 		Scanner keyboardInput = new Scanner(System.in);
 		System.out.println("Enter the number of the day of the week : ");
-		System.out.println("The day is : " + getDay(keyboardInput.nextInt()));
+		System.out.println("The day is : " + getDayIf(keyboardInput.nextInt()));
 	}
-	public static String getDay(int day){
+	public static String getDayIf(int day){
 
 		if (day==1){
 			return "Monday";
@@ -53,17 +54,36 @@ public class Main {
 		else{
 			return "";
 		}
-        /*
-        switch (day){
-            case 1: return "Monday";
-            case 2: return "Tuesday";
-            case 3: return "Wednesday";
-            case 4: return "Thursday";
-            case 5: return "Friday";
-            case 6: return "Saturday";
-            case 7: return "Sunday";
-            default: return "";
-        }
-        */
+	}
+
+
+	//ex3
+	public static void ex3(){
+		Scanner keyboardInput = new Scanner(System.in);
+		System.out.println("Enter the number of the day of the week : ");
+		System.out.println("The day is : " + getDayIf(keyboardInput.nextInt()));
+	}
+	public static String getDaySwitch(int day){
+		switch (day){
+			case 1: return "Monday";
+			case 2: return "Tuesday";
+			case 3: return "Wednesday";
+			case 4: return "Thursday";
+			case 5: return "Friday";
+			case 6: return "Saturday";
+			case 7: return "Sunday";
+			default: return "";
+		}
+	}
+
+
+	//ex4
+	public static void ex4(){
+		Scanner keyboardInput = new Scanner(System.in);
+		Hashtable<String,List<String>> days=new Hashtable<>();
+		days.put("English", Arrays.asList("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"));
+		days.put("French", Arrays.asList("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"));
+		System.out.println("Enter language and desired day of the week : ");
+		System.out.println(days.get(keyboardInput.nextLine()).get(keyboardInput.nextInt()-1));
 	}
 }
